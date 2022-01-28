de66f11daeefb496131fb1dbffb22b1b3233415b
halit özdemir <halit.ozdemir@outlook.com>
2022-01-28 12:47:26 +0300

    ilk commit

---
+++ DenemeCalismasi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DenemeCalismasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
