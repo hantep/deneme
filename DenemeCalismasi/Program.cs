50ef6c25f220e61861d2e68c3ffd6311429b3ccc
halit özdemir <halit.ozdemir@outlook.com>
2022-01-30 13:29:29 +0300

    testtt

--- DenemeCalismasi/Program.cs
+++ DenemeCalismasi/Program.cs
@@ -6,7 +6,7 @@ namespace DenemeCalismasi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
